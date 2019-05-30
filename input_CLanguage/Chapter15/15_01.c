d18d99514cc478df3b8ec0b78a9f0e661c33fb2b
Victor <wjdupsmj@hotmail.com>
2019-05-31 00:06:04 +0800

    Chapter 15 Practice

--- input_CLanguage/Chapter15/15_01.c
+++ input_CLanguage/Chapter15/15_01.c
@@ -1,3 +1,30 @@
 #include<stdio.h>
 #include<string.h>
+int de (int in);
 
+int main(void)
+{
+	char * pbin = "01001001";
+	int num = 0;
+	char ch;
+
+	for(int i = 0; i < 8; i++)
+	{
+		ch = pbin[i];
+		num += (int)(ch - '0') * de(7 - i);
+	}
+	printf("01001001 is %d\n", num);
+	return 0;
+}
+
+int de (int in)
+{
+	int de = 1;
+	if(in == 0)
+		de = 1;
+	else
+		for(int i = 0; i < in; i++)
+			de *= 2;
+	return de;
+
+}
